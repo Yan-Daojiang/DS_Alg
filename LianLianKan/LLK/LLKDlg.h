c3b023c41d09f59db26cc9e84695deca6ff30c26
Yan-Daojiang <1373836603@qq.com>
2019-05-20 22:16:09 +0800

    加载背景 添加几个按钮

---
+++ LianLianKan/LLK/LLKDlg.h
@@ -0,0 +1,36 @@
+﻿
+// LLKDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CLLKDlg 对话框
+class CLLKDlg : public CDialogEx
+{
+// 构造
+public:
+	CLLKDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_LLK_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+	CDC m_dcMem;
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+
+	void InitBackground();
+};
---
+++ LianLianKan/LLK/resource.h
@@ -0,0 +1,28 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 LLK.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_LLK_DIALOG                  102
+#define IDR_MAINFRAME                   128
+#define IDB_MAIN_BG                     131
+#define IDC_BUTTON_BAISC                1000
+#define IDC_BUTTON_RELAX                1001
+#define IDC_BUTTON_LEVEL                1002
+#define IDC_BUTTON_RANK                 1003
+#define IDC_BUTTON_HELP                 1004
+#define IDC_BUTTON_SETTING              1005
+#define IDC_PROGRESS1                   1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
